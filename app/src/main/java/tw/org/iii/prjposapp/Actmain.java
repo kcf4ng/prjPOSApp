e9bf4769add2f738d81c4dc5d94ab8cbcefe5d03
kcf4ng <kcf4ng@gmail.com>
2018-12-19 14:31:46 +0800

    20181219 commit spinner 糖冰杯數，完成

--- app/src/main/java/tw/org/iii/prjposapp/Actmain.java
+++ app/src/main/java/tw/org/iii/prjposapp/Actmain.java
@@ -13,12 +13,11 @@ public class Actmain extends AppCompatActivity {
 
 
         NumberPicker numberPicker = (NumberPicker) findViewById(R.id.picker);
-        final String[] miaoliStation = getResources().getStringArray(R.array.miaoli_station);
-
+        final String[] sugar = getResources().getStringArray(R.array.sugar);
         numberPicker.setMinValue(0);
-        numberPicker.setMaxValue(miaoliStation.length - 1);
-        numberPicker.setDisplayedValues(miaoliStation);
-        numberPicker.setValue(3); // 設定預設位置
+        numberPicker.setMaxValue(sugar.length - 1);
+        numberPicker.setDisplayedValues(sugar);
+        numberPicker.setValue(1); // 設定預設位置
         numberPicker.setWrapSelectorWheel(false); // 是否循環顯示
         numberPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); // 不可編輯
         numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
@@ -27,5 +26,42 @@ public class Actmain extends AppCompatActivity {
 
             }
         });
+
+        NumberPicker np2 =(NumberPicker) findViewById(R.id.picker2);
+        final String[] ice = getResources().getStringArray(R.array.ice);
+        np2.setMinValue(0);
+        np2.setMaxValue(ice.length -1);
+        np2.setDisplayedValues(ice);
+        np2.setValue(1);
+        np2.setWrapSelectorWheel(false);
+        np2.setDescendantFocusability(np2.FOCUS_BLOCK_DESCENDANTS);
+        np2.setOnValueChangedListener(new NumberPicker.OnValueChangeListener(){
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal){
+
+            }
+
+        });
+
+        NumberPicker np3 =(NumberPicker) findViewById(R.id.picker3);
+        np3.setMinValue(1);
+        np3.setMaxValue(99);
+        np3.setValue(1);
+        np3.setWrapSelectorWheel(false);
+        np3.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 }
